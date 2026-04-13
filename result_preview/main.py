47fb1ef0051ed3a78683bb239cac6e97abbde57e
Matthias Ebert <Matthias.Ebert+msggroup@msg.group>
2026-04-13 18:03:36 +0200

    Fix default user usage

--- result_preview/main.py
+++ result_preview/main.py
@@ -153,16 +153,19 @@ def generate_user_data():
     if(username != "passwort"):
         return username,password,pw_hash
 
+    stats = load_stats()
+
     index = 1
-    username = f"{username}{index}"
-    indexed_user = False
-    while(not indexed_user):
-        data = load_user_state(indexed_user, DEFAULT_TEAMS)
-        if not data or not username in data:
+    indexed_user_name = f"{username}{index}"
+    username_found = False
+    while(not username_found):
+        print(index)
+        print(indexed_user_name)
+        if stats and indexed_user_name in stats:
             index = index + 1
-            username = f"{username}{index}"
+            indexed_user_name = f"{username}{index}"
         else:
-            indexed_user = True
+            username_found = True
 
     return username,password,pw_hash
  
